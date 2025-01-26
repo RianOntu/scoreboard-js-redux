6088c2b0be896da53fab00588e45b07ae145198b
RianOntu <rianontu13@gmail.com>
2025-01-26 13:56:19 +0600

    completed the project

--- script.js
+++ script.js
@@ -1,7 +1,70 @@
+const addMatchButton = document.querySelector(".lws-addMatch");
+const allMatchesContainer = document.querySelector(".all-matches");
+
+addMatchButton.addEventListener("click", () => {
+  const newMatch = document.createElement("div");
+  newMatch.classList.add("match");
+
+  newMatch.innerHTML = `
+    <div class="wrapper">
+      <button class="lws-delete">
+        <img src="./image/delete.svg" alt="" />
+      </button>
+      <h3 class="lws-matchName">Match ${
+        allMatchesContainer.children.length + 1
+      }</h3>
+    </div>
+    <div class="inc-dec">
+      <form class="incrementForm">
+        <h4>Increment</h4>
+        <input type="number" name="increment" class="lws-increment" />
+        <input class="submit" type="submit" value="submit">
+      </form>
+      <form class="decrementForm">
+        <h4>Decrement</h4>
+        <input type="number" name="decrement" class="lws-decrement" />
+        <input class="submit" type="submit" value="submit">
+      </form>
+    </div>
+    <div class="numbers">
+      <h2 class="lws-singleResult">0</h2>
+    </div>
+  `;
+
+  allMatchesContainer.appendChild(newMatch);
+
+  const incrementForm = newMatch.querySelector(".incrementForm");
+  const decrementForm = newMatch.querySelector(".decrementForm");
+  const resultElement = newMatch.querySelector(".lws-singleResult");
+
+  incrementForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const incrementValue =
+      parseInt(incrementForm.querySelector(".lws-increment").value) || 0;
+    resultElement.innerText =
+      parseInt(resultElement.innerText) + incrementValue;
+  });
+
+  decrementForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const decrementValue =
+      parseInt(decrementForm.querySelector(".lws-decrement").value) || 0;
+    resultElement.innerText =
+      parseInt(resultElement.innerText) - decrementValue;
+  });
+
+  const deleteButton = newMatch.querySelector(".lws-delete");
+  deleteButton.addEventListener("click", () => {
+    newMatch.remove();
+  });
+});
+
 const incrementForm = document.getElementById("increment_form");
 const decrementForm = document.getElementById("decrement_form");
 const result = document.getElementById("result");
+const dynamicResult = document.querySelector(".lws-singleResult");
 const reset = document.getElementById("reset");
+const mainDeleteBtn = document.querySelector(".lws-delete");
 
 const initState = {
   value: 0,
@@ -30,6 +93,7 @@ const store = Redux.createStore(counterReducer);
 const render = () => {
   const resultValue = store.getState();
   result.innerText = resultValue.value;
+  dynamicResult.innerText = resultValue.value;
 };
 render();
 store.subscribe(render);
@@ -52,4 +116,25 @@ reset.addEventListener("click", () => {
   document.querySelector(".lws-increment").value = "";
   document.querySelector(".lws-decrement").value = "";
   document.getElementById("result").innerText = 0;
+
+  mainDeleteBtn.addEventListener("click", () => {
+    document.querySelector(".match").remove();
+  });
+
+  const allResults = allMatchesContainer.querySelectorAll(".lws-singleResult");
+  const allIncrementFields =
+    allMatchesContainer.querySelectorAll(".lws-increment");
+  console.log(allIncrementFields);
+
+  const allDecrementFields =
+    allMatchesContainer.querySelectorAll(".lws-decrement");
+  allResults.forEach((result) => {
+    result.innerText = 0;
+  });
+  allIncrementFields.forEach((increment) => {
+    increment.value = "";
+  });
+  allDecrementFields.forEach((decrement) => {
+    decrement.value = "";
+  });
 });
